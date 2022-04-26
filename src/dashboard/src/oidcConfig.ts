2892b894fb821dcf6c0dd80a3f2fef15989b7d88
f.steinhart <f.steinhart@futura-solutions.de>
2022-04-26 16:02:03 +0200

    Change userinfo_endpoint

--- src/dashboard/src/oidcConfig.ts
+++ src/dashboard/src/oidcConfig.ts
@@ -1,6 +1,12 @@
 import { AuthProviderProps } from 'react-oidc-context';
 import { resetUrl } from '@helpers';
 
+function getUserinfoEndpoint() {
+  return process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/account/@me`
+    : 'https://discord.com/api/users/@me';
+}
+
 export const oidcConfig: AuthProviderProps = {
   authority: 'https://discord.com/',
   client_id: String(process.env.REACT_APP_DC_CLIENT_ID),
@@ -11,7 +17,7 @@ export const oidcConfig: AuthProviderProps = {
     authorization_endpoint: 'https://discord.com/api/oauth2/authorize',
     token_endpoint: 'https://discord.com/api/oauth2/token',
     revocation_endpoint: 'https://discord.com/api/oauth2/token/revoke',
-    userinfo_endpoint: 'https://discord.com/api/users/@me',
+    userinfo_endpoint: getUserinfoEndpoint(),
     // Discord does not provide a end session endpoint so we set this to the app url.
     end_session_endpoint: window.location.origin,
   },

1dd393dcb6d285d75b6c1e531bb68d25f4c238a6
Felix <f.steinhart@futura-solutions.de>
2022-05-09 10:33:56 +0200

    Add comments

--- src/bot/services/settings_service.py
+++ src/bot/services/settings_service.py
@@ -6,6 +6,10 @@ from models import ServerSettings, ServerSettingsValues
 
 
 class SettingsService:
+    # DE: Initialisiert die Einstellungen für die angegebenen Server. Falls auf der Datenbank für den jeweiligen Server
+    #     noch keine Einstellungen hinterlegt sind, werden diese automatisch mit den Standardwerten (siehe models/server_settings.py) angelegt.
+    # EN: Initializes the settings for the specified servers. If no settings are stored in the database
+    #     for the respective server, they are automatically created with the default values (see models/server_settings.py).
     async def initialize(self, guild_ids: List[int] | int, integrity_check=True):
         server_settings_new = []
         server_settings_old = []
@@ -34,20 +38,30 @@ class SettingsService:
         if len(server_settings_old) > 0:
             await ServerSettings.find(In(ServerSettings.guild_id, server_settings_old)).delete_many()
 
+    # DE: Liefert alle Einstellungen für den angegebenen Server.
+    # EN: Returns all settings for the specified server.
     async def get(self, guild_id: int):
         return await self.__get_server_settings(guild_id)
 
+    # DE: Aktualisiert eine Einstellungswert für den angegebenen Server.
+    # EN: Updates a setting value for the specified server.
     async def set(self, guild_id: int, key: str, value):
         server_settings = await self.__get_server_settings(guild_id)
         setattr(server_settings.values, key, value)
         await server_settings.save()
 
+    # DE: Prüft, für welche Server bereits Einstellungen gespeichert wurden und gibt die Server IDs als Liste zurück.
+    # EN: Checks for which servers settings have already been saved and returns the server IDs as a list.
     async def which(self):
         server_settings = await ServerSettings.find().to_list()
         return [settings.guild_id for settings in server_settings]
 
+    # DE: Erstellt ein neues 'ServerSettings' Objekt und gibt es zurück. Das Objekt wird zu diesem Zeitpunkt noch nicht auf der Datenbank gespeichert.
+    # EN: Creates a new 'ServerSettings' object and returns it. The object is not yet stored in the database at this point.
     def __get_new(self, guild_id: int):
         return ServerSettings(guild_id=guild_id, values=ServerSettingsValues())
 
+    # DE: Ermittelt den Datenbankeintrag für die angegebene Server ID.
+    # EN: Determines the database entry for the specified server id.
     async def __get_server_settings(self, guild_id: int, fetch_links: bool = True):
         return await ServerSettings.find_one(ServerSettings.guild_id == guild_id, fetch_links=fetch_links)
--- src/bot/services/warn_service.py
+++ src/bot/services/warn_service.py
@@ -6,6 +6,8 @@ from models import Warn, WarnDetails
 
 
 class WarnService:
+    # DE: Verwarnt den angegebenen Benutzer.
+    # EN: Warns the specified user.
     async def add_warning(
         self,
         guild_id: int,
@@ -33,10 +35,14 @@ class WarnService:
 
         return len(warn.entries)
 
+    # DE: Ermittelt alle Verwarnungen für den angegebenen Benutzer.
+    # EN: Gets all warnings for the specified user.
     async def get_warnings(self, guild_id: int, member_id: int):
         warn = await self.__get_warn(guild_id, member_id)
         return warn.entries if warn else None
 
+    # DE: Entfernt eine oder mehrere Verwarnung für den angegebenen Benutzer.
+    # EN: Removes one or more warnings for the specified user.
     async def remove_warning(self, guild_id: int, member_id: int, warns_to_remove):
         warns_to_remove = " ".join(warns_to_remove.split())
         warn = await self.__get_warn(guild_id, member_id)
@@ -49,7 +55,7 @@ class WarnService:
         elif warns_to_remove.isdigit():
             self.__remove(warn.entries, warns_to_remove)
 
-        elif any(s in warns_to_remove for s in separators):
+        if any(s in warns_to_remove for s in separators):
             warns_to_remove = self.__args_to_list(warns_to_remove, separators)
             for number in reversed(sorted(warns_to_remove)):
                 self.__remove(warn.entries, number)
@@ -61,19 +67,21 @@ class WarnService:
 
         return len(warn.entries), warn_count_before - len(warn.entries)
 
+    # DE: Ermittelt den Datenbankeintrag für die Kombination Server ID / Member ID.
+    # EN: Determines the database entry for the combination server id / member id.
     async def __get_warn(self, guild_id: int, member_id: int, fetch_links: bool = True) -> Warn:
         return await Warn.find_one(Warn.guild_id == guild_id, Warn.member_id == member_id, fetch_links=fetch_links)
 
+    # DE: Wandelt die eingegebenen Argumente in eine Liste um.
+    # EN: Converts the entered arguments into a list.
     def __args_to_list(self, args, separators):
-        args = " ".join(args.split())
-
         for char in separators:
-            if char in args:
-                args = "".join(args.split())
-                return args.split(char)
+            args = args.replace(char, ";")
 
-        return None
+        return args.split(";") if args else None
 
+    # DE: Entfernt einen Eintrag aus einer Liste anhand seiner Position.
+    # EN: Removes an entry from a list based on its position.
     def __remove(self, warnings, number):
         index = int(number) - 1
         warnings.pop(index)

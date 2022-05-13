587d4aa70ffdeaba254191721564902b217acaf1
Felix <f.steinhart@futura-solutions.de>
2022-05-13 11:00:18 +0200

    Remove ticket service mock

--- src/bot/services/mock/ticket_service_mock.py
+++
@@ -1,8 +0,0 @@
-tickets_metadata_mock = [
-    # {
-    #     "server_id": 873898854607650826,
-    #     "category": "Tickets"
-    # }
-]
-
-tickets_mock = {}
--- src/bot/services/ticket_service.py
+++ src/bot/services/ticket_service.py
@@ -1,14 +1,2 @@
-from .mock.ticket_service_mock import tickets_metadata_mock, tickets_mock
-
-
 class TicketService:
-    def config(self, guild_id: int, category: str):
-        guild_metadata_entry = next(filter(lambda x: x["guild_id"] == guild_id, tickets_metadata_mock), None)
-
-        if not guild_metadata_entry:
-            tickets_metadata_mock.append({"guild_id": guild_id, "category": category})
-        else:
-            guild_metadata_entry["category"] = category
-
-    def get_config(self, guild_id):
-        return next(filter(lambda x: x["guild_id"] == guild_id, tickets_metadata_mock), None)
+    ...

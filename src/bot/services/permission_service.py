f5147b0da1083265dcf7881f5787e5759c903747
Felix <f.steinhart@futura-solutions.de>
2022-05-13 11:01:54 +0200

    Add comments

--- src/bot/services/permission_service.py
+++ src/bot/services/permission_service.py
@@ -3,6 +3,8 @@ from models.permissions import PermissionLevel
 
 
 class PermissionService:
+    # DE: Ermittelt die Berechtiungs-Stufe des Members mit der übergebenen ID.
+    # EN: Determines the permission level of the member with the given ID.
     async def get_permission_level(self, member_id: int):
         permissions = await Permissions.find_one(Permissions.member_id == member_id)
         return await permissions.level if permissions else PermissionLevel.NONE

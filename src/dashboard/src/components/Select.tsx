52beb867b141bad43d6a9a3e18c482d5446b8d02
Felix <f.steinhart@futura-solutions.de>
2022-05-16 16:54:07 +0200

    Remove comments

--- src/dashboard/src/components/Select.tsx
+++ src/dashboard/src/components/Select.tsx
@@ -3,12 +3,10 @@ import styled from 'styled-components';
 
 import { FormField } from './common/FormField';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {}
 
 const StyledSelect = styled.select``;
 
-// eslint-disable-next-line no-empty-pattern
 export function Select({ children, ...props }: SelectProps) {
   return (
     <FormField>

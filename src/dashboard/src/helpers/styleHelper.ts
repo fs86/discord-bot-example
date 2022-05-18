c63c8a3306ef14c7ca8429062f5aa2afa93df3f7
Felix Steinhart <f.steinhart@fsnet.eu>
2022-05-18 22:35:53 +0200

    Refactor styleHelper

--- src/dashboard/src/helpers/styleHelper.ts
+++ src/dashboard/src/helpers/styleHelper.ts
@@ -1,11 +1,9 @@
-export function hexToRGBA(hex: string, alpha: number) {
-  var r = parseInt(hex.slice(1, 3), 16),
-    g = parseInt(hex.slice(3, 5), 16),
-    b = parseInt(hex.slice(5, 7), 16);
+export function hexToRGBA(hex: string, alpha?: number) {
+  alpha = alpha || 1;
 
-  if (alpha) {
-    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
-  } else {
-    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-  }
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 }

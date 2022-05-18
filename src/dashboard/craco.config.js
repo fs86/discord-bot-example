26ede5c96ded5afd912a580d5a7d4b84f7884d68
Felix <f.steinhart@futura-solutions.de>
2022-05-18 10:18:08 +0200

    Add theme overrides

--- src/dashboard/craco.config.js
+++ src/dashboard/craco.config.js
@@ -19,7 +19,7 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
+            // modifyVars: { '@primary-color': '#1DA57A' },
             javascriptEnabled: true,
           },
         },

7add79373d16ebada5c1740732381d49751a4b69
Felix <f.steinhart@futura-solutions.de>
2022-06-03 15:17:29 +0200

    Change NotImplemented component

--- src/dashboard/src/components/common/NotImplemented.tsx
+++ src/dashboard/src/components/common/NotImplemented.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-rows: repeat(2, min-content);
+  grid-template-rows: repeat(3, min-content);
   border: 2px solid yellow;
   border-radius: 5px;
   white-space: pre-line;
@@ -14,21 +14,42 @@ const Header = styled.div`
   font-size: 14pt;
   padding: 0 6px;
   background-color: rgba(255, 255, 0, 0.2);
-  border-bottom: 1px solid yellow;
 `;
 
 const Content = styled.div`
-  padding: 6px;
+  padding: 3px 6px;
   background-color: rgba(255, 255, 0, 0.1);
+`;
+
+const Footer = styled.div`
+  padding: 3px 6px;
+  background-color: rgba(255, 255, 0, 0.1);
+  border-top: 1px solid rgba(255, 255, 0, 0.2);
+`;
+
+const StyledLink = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export function NotImplemented() {
   const { t } = useTranslation('notImplemented');
+  const Separator = () => <> | </>;
 
   return (
     <Wrapper>
       <Header>{t('header')}</Header>
       <Content>{(t('content', { returnObjects: true }) as string[]).join(' ')}</Content>
+      <Footer>
+        <StyledLink href={t('docs.link')} target="_blank">
+          {t('docs.label')}
+        </StyledLink>
+        <Separator />
+        <StyledLink href={t('discord.link')} target="_blank">
+          {t('discord.label')}
+        </StyledLink>
+      </Footer>
     </Wrapper>
   );
 }

cafb4940b4464b8117ed73fdc7a78a28eba2d07d
Felix Steinhart <f.steinhart@fsnet.eu>
2022-07-24 10:20:35 +0200

    Remove ReactQueryDevtools

--- src/dashboard/src/main.tsx
+++ src/dashboard/src/main.tsx
@@ -21,7 +21,7 @@ root.render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </AuthProvider>
 );
